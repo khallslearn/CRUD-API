d87bd8ce8cadc537eeea6e8574169171440e63c6
KH <75588413+khallslearn@users.noreply.github.com>
2022-07-12 19:56:59 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const { request } = require('express')
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+  'kendrick':{
+   'age': 35,
+   'birthName': 'Kung-fu',
+   'birthLocation': "Los  Angelos "
+  },
+  'tupac':{
+   'age': 80,
+   'birthName': 'Pac',
+   'birthLocation': "Brooklyn"
+  },
+  'glover':{
+   'age': 37,
+   'birthName': 'Gambino',
+   'birthLocation': "CaliforniaS"
+  },
+  'unknown':{
+   'age': unknown,
+   'birthName': 'unknown',
+   'birthLocation': "unknown"
+  }
+  
+}
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+  })
+
+  app.get('/api/:name', (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if(rappers[rapperName]){
+      res.json(rappers[rapperName]).birthName
+    }else{
+      res.json(rappers['unknown'])
+    }
+  }) 
+
+app.listen(PORT , () =>{
+    console.log(`The server is running on ${PORT}`)
+})
